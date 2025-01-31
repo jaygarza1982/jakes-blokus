e8c0fd6cd91e01a3e4ab38f61bfa56ffff3ae3aa
Jake Garza <jaygarza1982@gmail.com>
2025-01-31 13:02:55 -0700

    Only selected blocks can be moved and only 1 selected block at one time

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Canvas from './Canvas';
 import { useSetRecoilState } from 'recoil';
 import { GameDataAtom } from './atoms/GameData';
 import createPlayerBlock from './util/CreateBlock';
+import { GameData } from './GameData';
+import placePlayerBlock from './util/PlaceBlock';
 
 const App: React.FC = () => {
-  const setGameData = useSetRecoilState(GameDataAtom)
+  const setGameData = useSetRecoilState<GameData>(GameDataAtom);
 
   const addPlayerBlock = (playerId: string, shape: number[][]) => {
     const x = 0;
@@ -18,10 +20,17 @@ const App: React.FC = () => {
     }
   }
 
+  const placeSelectedBlock = () => {
+    setGameData(g => {
+      return placePlayerBlock(g, g.blocks.find(b => b.selected == true));
+    })
+  }
+
   return (
     <>
       <Canvas />
       <br />
+      <button onClick={placeSelectedBlock}>Place Block</button>
       <button onClick={addPlayerBlock('Player 1', [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]])}>Add player 1 L block</button>
       <button onClick={addPlayerBlock('Player 1', [])}>Add player 1 T block</button>
       <button onClick={addPlayerBlock('Player 2', [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]])}>Add player 2 L block</button>
--- src/util/CreateBlock.ts
+++ src/util/CreateBlock.ts
@@ -1,7 +1,10 @@
 import { GameData } from '../GameData';
 import { v4 as uuid } from 'uuid';
 
-const createPlayerBlock = (g: GameData, playerId: string, shape: number[][], x: number, y: number) => {
+const createPlayerBlock = (g: GameData, playerId: string, shape: number[][], x: number, y: number): GameData => {
+  // Do not allow players to create more than 1 block at a time
+  if (g.blocks.find(b => b.selected)) return g;
+
   return {
     players: g.players,
     blocks: [...g.blocks, {
@@ -9,7 +12,8 @@ const createPlayerBlock = (g: GameData, playerId: string, shape: number[][], x: 
       playerId: playerId,
       shape: shape,
       x: x,
-      y: y
+      y: y,
+      selected: true
     }]
   }
 }
