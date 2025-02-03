fb4df28eebbefcde563fd7d4386e6af4357468ef
Jake Garza <jaygarza1982@gmail.com>
2025-02-03 09:32:09 -0700

    Swap selected block if placing new

--- src/util/CreateBlock.ts
+++ src/util/CreateBlock.ts
@@ -2,17 +2,21 @@ import { GameData } from '../GameData';
 import { v4 as uuid } from 'uuid';
 
 const createPlayerBlock = (g: GameData, playerId: string, shape: number[][], x: number, y: number): GameData => {
-  // Do not allow players to create more than 1 block at a time
-  if (g.blocks.find(b => b.selected)) return g;
+  // Swap selected block with new selected block if one already exists
+  // Do not allow multiple selected blocks
+  const placedBlocks = g.blocks.filter(b => !b.selected);
+
+  const selectedBlock = g.blocks.find(b => b.selected);
 
   return {
     players: g.players,
-    blocks: [...g.blocks, {
+    blocks: [...placedBlocks, {
       blockId: uuid(),
       playerId: playerId,
       shape: shape,
-      x: x,
-      y: y,
+      // Keep selected block in same position if we have a selected block already
+      x: selectedBlock?.x ?? x,
+      y: selectedBlock?.y ?? y,
       selected: true
     }]
   }
