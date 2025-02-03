2090b2abd03251bd53711d385df10b7b4a8348b1
Jake Garza <jaygarza1982@gmail.com>
2025-02-03 12:05:31 -0700

    Cannot place over existing blocks

--- src/util/PlaceBlock.ts
+++ src/util/PlaceBlock.ts
@@ -1,20 +1,33 @@
 import { Block, GameData } from '../GameData';
 
+const gridSize = 50;
+const width = 500;
+const height = 500;
+
+const getBlockGridPositions = (block: Block): number[][] => {
+  return block.shape.map(
+    p => [(p[0] * gridSize) + block.x, (p[1] * gridSize) + block.y]
+  );
+}
+
 // Checks for in game bounds
 // TODO: Check that not touching same player-owned blocks in directly left, right, up, and down
 // TODO: Placing block must touch diagonal player-owned block at least once
 const canPlaceBlock = (placedBlocks: Block[], block: Block): boolean => {
-  const gridSize = 50;
-  const width = 500;
-  const height = 500;
+  const blockGridPositions = getBlockGridPositions(block);
 
-  const shapeGridPositions = block.shape.map(
-    p => [(p[0] * gridSize) + block.x, (p[1] * gridSize) + block.y]
-  );
-  const positionsInBounds = shapeGridPositions.map(p => !(p[0] < 0 || p[0] >= width || p[1] < 0 || p[1] >= height));
+  const positionsInBounds = blockGridPositions.map(p => !(p[0] < 0 || p[0] >= width || p[1] < 0 || p[1] >= height));
   const allInBounds = positionsInBounds.every(b => b === true);
-  
-  return allInBounds;
+
+  // Check that blocks do not overlap
+  // Make a list of strings that contain positions of all placed blocks and block we are placing
+  // The positions should all be unique. If they are not, we have overlap
+  const blockPositionStrings: string[] = blockGridPositions.map(i => JSON.stringify(i));
+  const placedPositionStrings: string[] = placedBlocks.map(b => getBlockGridPositions(b)).flat().map(i => JSON.stringify(i));
+  const allPositionStrings = placedPositionStrings.concat(blockPositionStrings);  
+  const newBlockNotOverlappingExisting = allPositionStrings.length == new Set(allPositionStrings).size;
+
+  return allInBounds && newBlockNotOverlappingExisting;
 }
 
 const placePlayerBlock = (g: GameData, block: Block | undefined): GameData => {
